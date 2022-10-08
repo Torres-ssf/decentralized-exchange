b4035bc7b20ca72c821106335992bae332f331df
Torres-ssf <sergio.uft@gmail.com>
2022-10-08 14:10:55 -0300

    add some configs to hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,10 @@ import '@nomicfoundation/hardhat-toolbox'
 import 'dotenv/config'
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.17',
+  solidity: '0.8.9',
+  networks: {
+    localhost: {},
+  }
 }
 
 export default config
