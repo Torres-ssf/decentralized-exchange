b3442c666111e18ff718e8544bfc49369ee9414b
Torres-ssf <sergio.uft@gmail.com>
2022-10-09 10:46:27 -0300

    add tranfer function to token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,9 +9,25 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply)  {
+    mapping(address => uint256) public balanceOf;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    )  {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // require(balanceOf[msg.sender] >= _value, "Not enough tokens");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        return true;
     }
 }
--- test/Token.spec.ts
+++ test/Token.spec.ts
@@ -1,25 +1,34 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { expect } from "chai"
+import { parseUnits } from "ethers/lib/utils"
 import { ethers } from "hardhat"
 import { Token } from "../typechain-types"
 
 describe('Token', () => {
 
+  let token: Token
+  let accounts: SignerWithAddress[]
+  let deployer: SignerWithAddress
+  let receiver: SignerWithAddress
+
+  const name = 'Torres Token'
+  const symbol = 'TT'
+  const decimals = 18
+  const totalSupply = 1000000
+
+  beforeEach(async () => {
+
+    const Token = await ethers.getContractFactory('Token')
+
+    token = await Token.deploy(name, symbol, totalSupply)
+
+    accounts = await ethers.getSigners();
+
+    ([deployer, receiver] = accounts)
+
+  })
+
   describe('Deployment', () => {
-
-    let token: Token
-
-    const name = 'Torres Token'
-    const symbol = 'TT'
-    const decimals = 18
-    const totalSupply = 1000000
-
-    before( async () => {
-
-      const Token = await ethers.getContractFactory('Token')
-
-      token = await Token.deploy(name, symbol, totalSupply)
-
-    })
 
     it('should ensure token name is correct', async () => {
       expect(await token.name()).to.be.eq(name)
@@ -34,11 +43,47 @@ describe('Token', () => {
     })
 
     it('should ensure token totalSupply is correct', async () => {
-      const parsedUnits = ethers.utils.parseUnits(String(totalSupply), decimals)
-      expect(await token.totalSupply()).to.be.eq(parsedUnits)
+      const parsedTotalSupply = parseTokenUnits(totalSupply)
+      expect(await token.totalSupply()).to.be.eq(parsedTotalSupply)
+    })
+
+    it('should assign totalSupply to the deployer', async () => {
+      const parsedTotalSupply = parseTokenUnits(totalSupply)
+      expect(await token.balanceOf(deployer.address)).to.be.eq(parsedTotalSupply)
     })
 
   })
 
+  describe('Sending tokens', () => {
+
+    it('should successfully tranfer tokens', async () => {
+
+      let amount = 100
+
+      let deployerBalance = await token.balanceOf(deployer.address)
+      let receiverBalance = await token.balanceOf(receiver.address)
+
+      expect(deployerBalance).to.be.eq(parseTokenUnits(totalSupply))
+      expect(receiverBalance).to.be.eq(0)
+
+      const transaction = await token
+        .connect(deployer)
+        .transfer(receiver.address, parseTokenUnits(amount))
+
+      await transaction.wait()
+
+      deployerBalance = await token.balanceOf(deployer.address)
+      receiverBalance = await token.balanceOf(receiver.address)
+
+      expect(deployerBalance).to.be.eq(parseTokenUnits(totalSupply - amount))
+      expect(receiverBalance).to.be.eq(parseTokenUnits(amount))
+
+    })
+
+  })
+
+  const parseTokenUnits = (amount: number) => {
+    return parseUnits(String(amount))
+  }
 
 })
