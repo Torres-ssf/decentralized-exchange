1bf044981b934c131a285180469b24516b3115f9
Torres-ssf <sergio.uft@gmail.com>
2022-10-17 20:28:46 -0300

    set up token and exchange contract deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,18 +2,25 @@ import { ethers } from 'hardhat'
 
 async function main(): Promise<void> {
 
+  const accounts = await ethers.getSigners()
+
   const Token = await ethers.getContractFactory('Token')
+  const Exchange = await ethers.getContractFactory('Exchange')
 
-  const token = await Token.deploy()
+  const mUSDT = await Token.deploy('Moked USDT', 'mUSDT', '1000000')
+  await mUSDT.deployed()
+  console.info('Deployed mUSDT to: ', mUSDT.address)
 
-  await token.deployed()
+  const mWBTC = await Token.deploy('Mocked Wrapped BTC', 'mWBTC', '1000000')
+  await mWBTC.deployed()
+  console.info('Deployed mWBTC to: ', mWBTC.address)
 
-  console.log(`Token address is: ${token.address}`)
-
+  const exchange = await Exchange.deploy(accounts[1].address, 1)
+  await exchange.deployed()
+  console.info('Deployed Exchange to: ', exchange.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch(error => {
   console.error(error)
   process.exitCode = 1
